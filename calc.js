70c7fafc2b26a43f94c6a1a53299a8a2cca6c533
Rieko Iijima <riekoiijima@gmail.com>
2017-11-19 21:35:51 +0900

    計算機ができること

--- calc.js
+++ calc.js
@@ -1,20 +1,6 @@
-$(function () {
-    var buttons = $("#btns > div"); 
-    var display = $("#display");
-    
-    buttons.click(function () {
-        var target = $(this);
-        var selectValue = target.attr("data-num");
-        
-        setDisplay(selectValue);
-    });
-    
-    function setDisplay(str) {
-        var beforeStr = display.text();
-        console.log(beforeStr, typeof(beforeStr));
-        console.log(str, typeof(str));
-        
-        var afterStr = beforeStr + str;
-        display.text(afterStr);
-    }
-});
+// 足し算
+// 引き算
+// 掛け算
+// 割り算
+// 数字を入力
+// 入力をクリア
